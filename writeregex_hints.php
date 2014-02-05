c3c27604a0ecae5a9d370b2f8e475267050daa72
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-02-05 15:34:59 +0400

    ? ????? ????????? ? ???? ????? ?????????? ???????? ????? ????????? ???? ?????????.

--- writeregex_hints.php
+++ writeregex_hints.php
@@ -171,6 +171,14 @@ class qtype_writeregex_explgraphhint extends qtype_specific_hint {
     }
 
     /**
+     * Get hint type.
+     * @return int Hint type.
+     */
+    public function hint_type() {
+        return qtype_specific_hint::SINGLE_INSTANCE_HINT;
+    }
+
+    /**
      * Render hint function.
      * @param question $renderer
      * @param question_attempt $qa
