5a19a4969e4b3125ee2ecd0fa77716e2da5677b8
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-02-05 17:23:08 +0400

    ? ????? ????????? ? ???? ?????????? ???????? ???????? ????? ????????? ???????? ??????.

--- writeregex_hints.php
+++ writeregex_hints.php
@@ -319,6 +319,15 @@ class qtype_writeregex_descriptionhint extends qtype_specific_hint {
     }
 
     /**
+     * Get penalty value.
+     * @param null $response object Response.
+     * @return float Value of current hint penalty.
+     */
+    public function penalty_for_specific_hint ($response = null) {
+        return $this->question->descriptionhintpenalty;
+    }
+
+    /**
      * Render hint function.
      * @param question $renderer
      * @param question_attempt $qa
