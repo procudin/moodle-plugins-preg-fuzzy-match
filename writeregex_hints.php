048181e934d9d8c8f4663e668cc124d3fb312052
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-02-05 17:16:17 +0400

    ? ????? ????????? ? ???? ?????????? ???????? ???????? ????? ????????? ???? ?????????.

--- writeregex_hints.php
+++ writeregex_hints.php
@@ -295,6 +295,14 @@ class qtype_writeregex_descriptionhint extends qtype_specific_hint {
     }
 
     /**
+     * Get hint type.
+     * @return int hint type.
+     */
+    public function hint_type() {
+        return qtype_specific_hint::SINGLE_INSTANCE_HINT;
+    }
+
+    /**
      * Render hint function.
      * @param question $renderer
      * @param question_attempt $qa
