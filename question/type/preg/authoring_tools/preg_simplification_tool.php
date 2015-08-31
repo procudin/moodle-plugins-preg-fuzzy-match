6e291d84190b8974f09a79f86be8d4e5d2c50cc4
gvterechov <gvterechov@localhost>
2015-09-01 00:35:32 +0300

    ????????? ???????? ? ???????? ?????? ?????? ?????? ??????? ????????

--- question/type/preg/authoring_tools/preg_simplification_tool.php
+++ question/type/preg/authoring_tools/preg_simplification_tool.php
@@ -22,6 +22,7 @@ class qtype_preg_simplification_tool extends qtype_preg_authoring_tool {
 
     private $problem_id = -2;
     private $problem_type = -2;
+    private $is_find_assert = false;
 
     public function __construct($regex = null, $options = null) {
         parent::__construct($regex, $options);
@@ -110,6 +111,13 @@ class qtype_preg_simplification_tool extends qtype_preg_authoring_tool {
 
         if ($this->options->is_check_equivalences == true) {
             $i = 0;
+            $result = $this->repeated_assertions();
+            if ($result != array()) {
+                $equivalences[$i] = array();
+                $equivalences[$i] += $result;
+                ++$i;
+            }
+
             $result = $this->grouping_node();
             if ($result != array()) {
                 $equivalences[$i] = array();
@@ -123,52 +131,67 @@ class qtype_preg_simplification_tool extends qtype_preg_authoring_tool {
                 $equivalences[$i] += $result;
                 ++$i;
             }
+
+            $result = $this->single_charset_node();
+            if ($result != array()) {
+                $equivalences[$i] = array();
+                $equivalences[$i] += $result;
+                ++$i;
+            }
         }
 
         return $equivalences;
     }
 
 
+    //--- CHECK RULES ---
+    /* The 1st rule */
+    protected function repeated_assertions() {
+        $equivalences = array();
 
-    //--- OPTIMIZATION ---
-    public function optimization() {
-        if ($this->options->problem_type == 2) {
-            return $this->optimize_2($this->get_dst_root());
-        } else if ($this->options->problem_type == 3) {
-            return $this->optimize_3();
-        }
-        return false;
-    }
-
-
-    // The 2th rule
-    protected function optimize_2($node) {
-        return remove_subtree($node, $this->options->problem_id);
-    }
-
-    // The 3th rule
-    protected function optimize_3($node) {
-        return remove_subtree($node, $this->options->problem_id);
-    }
-
-    private function remove_subtree($node, $remove_node_id) {
-        if ($node->id == $remove_node_id) {
-            return true;
+        if ($this->search_repeated_assertions($this->get_dst_root())) {
+            $equivalences['problem'] = 'Подряд идущие простые утверждения';
+            $equivalences['solve'] = 'Подрядищущее простое утверждения является лишним, т.к. рядом уже есть такое же';
+            $equivalences['problem_id'] = $this->problem_id;
+            $equivalences['problem_type'] = $this->problem_type;
         }
 
-        foreach($node->operands as $i => $operand) {
-            if ($this->remove_subtree($operand, $remove_node_id)) {
-                array_splice($node->operands, $i, 1);
+        return $equivalences;
+    }
+
+    private function search_repeated_assertions($node) {
+        if ($node->type == qtype_preg_node::TYPE_LEAF_ASSERT
+            && ($node->subtype == qtype_preg_leaf_assert::SUBTYPE_CIRCUMFLEX || $node->subtype == qtype_preg_leaf_assert::SUBTYPE_DOLLAR)) {
+            if ($this->is_find_assert == true) {
+                $this->problem_id = $node->id;
+                $this->problem_type = 1;
+                return true;
+            } else {
+                $this->is_find_assert = true;
+            }
+        } else if ($node->type == qtype_preg_node::TYPE_LEAF_CHARSET
+                   || $node->type == qtype_preg_node::TYPE_LEAF_META
+                   || $node->type == qtype_preg_node::TYPE_LEAF_BACKREF
+                   || $node->type == qtype_preg_node::TYPE_LEAF_SUBEXPR_CALL
+                   || $node->type == qtype_preg_node::TYPE_LEAF_TEMPLATE
+                   || $node->type == qtype_preg_node::TYPE_LEAF_CONTROL
+                   || $node->type == qtype_preg_node::TYPE_LEAF_OPTIONS
+                   || $node->type == qtype_preg_node::TYPE_LEAF_COMPLEX_ASSERT) {
+            $this->is_find_assert = false;
+        }
+        foreach($node->operands as $operand) {
+            if ($this->search_repeated_assertions($operand)) {
                 return true;
             }
         }
-
+        $this->problem_id = -2;
+        $this->problem_type = -2;
         return false;
     }
 
 
-    //--- CHECK RULES ---
-    /* The 2th rule */
+
+    /* The 2nd rule */
     protected function grouping_node() {
         $equivalences = array();
 
@@ -202,7 +225,7 @@ class qtype_preg_simplification_tool extends qtype_preg_authoring_tool {
 
 
 
-    /* The 3th rule */
+    /* The 3rd rule */
     protected function subpattern_node() {
         $equivalences = array();
 
@@ -246,5 +269,99 @@ class qtype_preg_simplification_tool extends qtype_preg_authoring_tool {
             }
         }
         return false;
+    }
+
+
+
+    /* The 5th rule */
+    protected function single_charset_node() {
+        $equivalences = array();
+
+        if ($this->search_single_charset_node($this->get_dst_root())) {
+            $equivalences['problem'] = 'Лишние квадратные скобки "[]"';
+            $equivalences['solve'] = 'Квадратные скобки можно убрать, т.к. в них всего один символ';
+            $equivalences['problem_id'] = $this->problem_id;
+            $equivalences['problem_type'] = $this->problem_type;
+        }
+
+        return $equivalences;
+    }
+
+    private function search_single_charset_node($node) {
+        if ($node->type == qtype_preg_node::TYPE_LEAF_CHARSET && $node->subtype == NULL) {
+            if ($node->is_single_character() == true && $node->negative == false) {
+                $this->problem_id = $node->id;
+                $this->problem_type = 5;
+                return true;
+            }
+        }
+        foreach($node->operands as $operand) {
+            if ($this->search_single_charset_node($operand)) {
+                return true;
+            }
+        }
+        $this->problem_id = -2;
+        $this->problem_type = -2;
+        return false;
+    }
+
+
+    //--- OPTIMIZATION ---
+    public function optimization() {
+        if ($this->options->problem_id != -2 && $this->options->problem_type != -2) {
+            $function_name = ('optimize_' . $this->options->problem_type);
+            return $this->$function_name($this->get_dst_root());
+        }
+        return false;
+    }
+
+
+    // The 1st rule
+    protected function optimize_1($node) {
+        return remove_subtree($node, $this->options->problem_id);
+    }
+
+    // The 2nd rule
+    protected function optimize_2($node) {
+        return remove_subtree($node, $this->options->problem_id);
+    }
+
+    // The 3rd rule
+    protected function optimize_3($node) {
+        return remove_subtree($node, $this->options->problem_id);
+    }
+
+    // The 5th rule
+    protected function optimize_5($node) {
+        return $this->remove_square_brackets_from_charset($node, $this->options->problem_id);
+    }
+
+    private function remove_subtree($node, $remove_node_id) {
+        if ($node->id == $remove_node_id) {
+            return true;
+        }
+
+        foreach($node->operands as $i => $operand) {
+            if ($this->remove_subtree($operand, $remove_node_id)) {
+                array_splice($node->operands, $i, 1);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function remove_square_brackets_from_charset($node, $remove_node_id) {
+        if ($node->id == $remove_node_id) {
+            return true;
+        }
+
+        foreach($node->operands as $operand) {
+            if ($this->remove_subtree($operand, $remove_node_id)) {
+                array_shift($operand->userinscription);
+                array_pop($operand->userinscription);
+                return true;
+            }
+        }
     }
 }
