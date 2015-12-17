4764a06288710ea9bce3fba62dcac8ad40a5d524
Kamo Spertsian <Kamo Spertsian@localhost>
2015-12-17 21:49:51 +0300

    ??????? ??????? ????? ????????? ? ????? ??????????.

--- question/type/writeregex/classes/syntax_tree_hint.php
+++ question/type/writeregex/classes/syntax_tree_hint.php
@@ -30,166 +30,56 @@ require_once($CFG->dirroot . '/question/type/preg/authoring_tools/preg_syntax_tr
  * @author Mikhail Navrotskiy <m.navrotskiy@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class syntax_tree_hint extends \qtype_poasquestion\hint {
+class syntax_tree_hint extends hint {
 
-    /** @var  int Mode of hint. */
-    protected $mode;
-    /** @var  object Object on question. */
-    protected $question;
-    /** @var  string Hint key. */
-    protected $hintkey;
-    /** @var array Options of syntax tree hint. */
-    protected $syntaxtreeoptions = array();
-
-    public function  set_mode ($value) {
-        $this->mode = $value;
+    /**
+     * Get hint title.
+     * @return string hint title.
+     */
+    public function hint_title() {
+        return get_string('wre_st', 'qtype_writeregex');
     }
 
     /**
-     * Init all fields.
-     * @param $question object Object of question's class.
-     * @param $hintkey string Hint key.
-     * @param $mode int Mode of current hint.
+     * @return string key for lang strings and field names
      */
-    public function __construct ($question, $hintkey, $mode) {
-
-        $this->question = $question;
-        $this->hintkey = $hintkey;
-        $this->mode = $mode;
-        $this->syntaxtreeoptions = array(
-            '0' => get_string('none', 'qtype_writeregex'),
-            '1' => get_string('student', 'qtype_writeregex'),
-            '2' => get_string('answer', 'qtype_writeregex'),
-            '3' => get_string('both', 'qtype_writeregex')
-        );
+    function short_key() {
+        return 'syntaxtree';
     }
 
     /**
-     * Get options of syntax tree hint.
-     * @return array Options of syntax tree hint.
+     * @return qtype_preg_authoring_tool tool used for hint
      */
-    public function syntaxtreeoptions() {
-        return $this->syntaxtreeoptions;
-    }
-
-    /**
-     * Get hint type.
-     * @return int hint type.
-     */
-    public function hint_type() {
-        return \qtype_poasquestion\hint::SINGLE_INSTANCE_HINT;
-    }
-
-    /**
-     * Get hint description.
-     * @return string hint description.
-     */
-    public function hint_description() {
-        // Description for hint using mode (e.g. '(for correct answer)')
-        $additionformode = get_string('hinttitleaddition', 'qtype_writeregex', get_string('hinttitleadditionformode_' . $this->mode, 'qtype_writeregex'));
-        return \qtype_poasquestion\string::strtolower(get_string('wre_st', 'qtype_writeregex')) . ' ' . $additionformode;
-    }
-
-    /**
-     * Get value of hint response based or not.
-     * @return bool hint response based.
-     */
-    public function hint_response_based() {
-        return $this->mode != 2;
-    }
-
-    /**
-     * Get penalty value.
-     * @param null $response object Response.
-     * @return float Value of current hint penalty.
-     */
-    public function penalty_for_specific_hint ($response = null) {
-        return $this->question->syntaxtreehintpenalty;
-    }
-
-    /**
-     * Return true if can available hint for answer.
-     * @param $answer User answer.
-     * @return bool Can available hint for answer?
-     */
-    public function can_available_hint_for_answer ($answer) {
-
-        if ($this->mode == 3) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    /**
-     * Get hint available.
-     * @param null $response Response.
-     * @return bool Hint available.
-     */
-    public function hint_available ($response = null) {
-
-        switch ($this->question->syntaxtreehinttype) {
-            case 0:
-                return false;
-            case 2:
-                return true;
-            default:
-                if ($response === null)
-                    return true;
-
-                // Check for possibility of using hint for current student response.
-                $regexoptions = new \qtype_preg_authoring_tools_options();
-                $regexoptions->engine = $this->question->engine;
-                $regexoptions->usecase = $this->question->usecase;
-                $regexoptions->notation = $this->question->notation;
-
-                $tree = new \qtype_preg_syntax_tree_tool($response['answer'], $regexoptions);
-                if (count($tree->get_errors()) > 0) {
-                    return false;
-                }
-                return true;
-        }
-        return true;
-    }
-
-    /**
-     * Render hint function.
-     * @param question $renderer
-     * @param question_attempt $qa
-     * @param question_display_options $options
-     * @param null $response
-     * @return string Template code value.
-     */
-    public function render_hint ($renderer, \question_attempt $qa = null,
-                                 \question_display_options $options = null, $response = null) {
+    function tool($regex) {
         $regexoptions = new \qtype_preg_authoring_tools_options();
         $regexoptions->engine = $this->question->engine;
         $regexoptions->usecase = $this->question->usecase;
         $regexoptions->notation = $this->question->notation;
-        $hinttitlestring = $renderer->render_hint_title(get_string('syntaxtreehintexplanation', 'qtype_writeregex',
-            get_string('hinttitleadditionformode_' . $this->mode, 'qtype_writeregex')));
+        return new \qtype_preg_syntax_tree_tool($regex, $regexoptions);
+    }
 
-        switch($this->mode){
-            case 1:
-                $tree = new \qtype_preg_syntax_tree_tool($response['answer'], $regexoptions);
-                $json = $tree->generate_json();
-                return $hinttitlestring . $json['tree']['img'];
-            case 2:
-                $answer = $this->question->get_best_fit_answer($response);
-                $tree = new \qtype_preg_syntax_tree_tool($answer['answer']->answer, $regexoptions);
-                $json = $tree->generate_json();
-                return $hinttitlestring . $json['tree']['img'];
-            case 3:
-                $tree = new \qtype_preg_syntax_tree_tool($response['answer'], $regexoptions);
-                $json = $tree->generate_json();
-                $answer = $this->question->get_best_fit_answer($response);
-                $tree2 = new \qtype_preg_syntax_tree_tool($answer['answer']->answer, $regexoptions);
-                $json2 = $tree2->generate_json();
-                return $hinttitlestring .
-                    get_string('hintdescriptionstudentsanswer', 'qtype_writeregex') . ':<br>' . $json['tree']['img'] . "<br>" .
-                    get_string('hintdescriptionteachersanswer', 'qtype_writeregex') . ':<br>' . $json2['tree']['img'];
-            default:
-                return '';
-        }
+    /**
+     * Render hint for concrete regex.
+     * @param string $regex regex for which hint is to be shown
+     * @return string hint display result for given regex.
+     */
+    public function render_hint_for_answer($answer) {
+        $tree = $this->tool($answer);
+        $json = $tree->generate_json();
+        return $json['tree']['img'];
+    }
+
+    /**
+     * Render hint for both students and teachers answers.
+     * @param string $studentsanswer students answer
+     * @param string $teachersanswer teachers answer
+     * @param question $renderer
+     * @return string hint display result for given answers.
+     */
+    public function render_hint_for_both_students_and_teachers_answers($studentsanswer, $teachersanswer, $renderer) {
+        $hintforstudent = $this->render_hint_for_answer($studentsanswer);
+        $hintforteacher = $this->render_hint_for_answer($teachersanswer);
+        return get_string('hintdescriptionstudentsanswer', 'qtype_writeregex') . ':<br>' . $hintforstudent . "<br>" .
+               get_string('hintdescriptionteachersanswer', 'qtype_writeregex') . ':<br>' . $hintforteacher;
     }
 }
--- question/type/writeregex/classes/test_strings_hint.php
+++ question/type/writeregex/classes/test_strings_hint.php
@@ -31,152 +31,27 @@ require_once($CFG->dirroot . '/question/type/preg/preg_matcher.php');
  * @author Mikhail Navrotskiy <m.navrotskiy@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class test_strings_hint extends \qtype_poasquestion\hint {
+class test_strings_hint extends hint {
 
-    /** @var  int Mode of hint. */
-    protected $mode;
-    /** @var  object Object on question. */
-    protected $question;
-    /** @var  string Hint key. */
-    protected $hintkey;
-    /** @var array Options of syntax tree hint. */
-    protected $teststringshintoptions = array();
-
-    public function  set_mode ($value) {
-        $this->mode = $value;
+    /**
+     * Get hint title.
+     * @return string hint title.
+     */
+    public function hint_title() {
+        return get_string('teststrings', 'qtype_writeregex');
     }
 
     /**
-     * Init all fields.
-     * @param $question object Object of question's class.
-     * @param $hintkey string Hint key.
-     * @param $mode int Mode of current hint.
+     * @return string key for lang strings and field names
      */
-    public function __construct ($question, $hintkey, $mode) {
-
-        $this->question = $question;
-        $this->hintkey = $hintkey;
-        $this->mode = $mode;
-        $this->teststringshintoptions = array(
-            '0' => get_string('none', 'qtype_writeregex'),
-            '1' => get_string('student', 'qtype_writeregex'),
-            '2' => get_string('answer', 'qtype_writeregex'),
-            '3' => get_string('both', 'qtype_writeregex')
-        );
+    function short_key() {
+        return 'teststrings';
     }
 
     /**
-     * Get options of test strings hint.
-     * @return array Options of test strings hint.
+     * @return qtype_preg_authoring_tool tool used for hint
      */
-    public function teststringshintoptions() {
-        return $this->teststringshintoptions;
-    }
-
-    /**
-     * Get hint type.
-     * @return int hint type.
-     */
-    public function hint_type() {
-        return \qtype_poasquestion\hint::SINGLE_INSTANCE_HINT;
-    }
-
-    /**
-     * Get hint description.
-     * @return string hint description.
-     */
-    public function hint_description() {
-        // Description for hint using mode (e.g. '(for correct answer)')
-        $additionformode = get_string('hinttitleaddition', 'qtype_writeregex', get_string('hinttitleadditionformode_' . $this->mode, 'qtype_writeregex'));
-        return \qtype_poasquestion\string::strtolower(get_string('teststrings', 'qtype_writeregex')) . ' ' . $additionformode;
-    }
-
-    /**
-     * Get value of hint response based or not.
-     * @return bool hint response based.
-     */
-    public function hint_response_based() {
-        return $this->mode != 2;
-    }
-
-    /**
-     * Get penalty value.
-     * @param null $response object Response.
-     * @return float Value of current hint penalty.
-     */
-    public function penalty_for_specific_hint ($response = null) {
-        return $this->question->teststringshintpenalty;
-    }
-
-    /**
-     * Return true if can available hint for answer.
-     * @param $answer User answer.
-     * @return bool Can available hint for answer?
-     */
-    public function can_available_hint_for_answer ($answer) {
-
-        if ($this->mode == 3) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    /**
-     * Get hint available.
-     * @param null $response Response.
-     * @return bool Hint available.
-     */
-    public function hint_available ($response = null) {
-
-        switch ($this->question->teststringshinttype) {
-            case 0:
-                return false;
-            case 2:
-                return true;
-            default:
-                if ($response === null)
-                    return true;
-
-                // Check for possibility of using hint for current student response.
-                $strings = '';
-                $key = 0;
-                foreach ($this->question->teststrings as $item) {
-
-                    if ($key == count($this->question->teststrings) - 1) {
-                        $strings .= $item->teststring;
-                    } else {
-                        $strings .= $item->teststring . "\n";
-                        $key++;
-                    }
-                }
-                $usecase = $this->question->usecase;
-                $exactmatch = false;
-                $engine = $this->question->engine;
-                $notation = $this->question->notation;
-                $regex = $response['answer'];
-
-                $tool = new \qtype_preg_regex_testing_tool($regex, $strings, $usecase, $exactmatch, $engine,
-                        $notation, new \qtype_preg_position());
-                if ($tool->errors_exist())
-                    return false;
-
-                return true;
-        }
-
-        return true;
-    }
-
-    /**
-     * Render hint function.
-     * @param question $renderer
-     * @param question_attempt $qa
-     * @param question_display_options $options
-     * @param null $response
-     * @return string Template code value.
-     */
-    public function render_hint($renderer, \question_attempt $qa = null, \question_display_options $options = null, $response = null) {
-
+    function tool($regex) {
         $strings = '';
         $key = 0;
         foreach ($this->question->teststrings as $item) {
@@ -188,41 +63,35 @@ class test_strings_hint extends \qtype_poasquestion\hint {
                 $key++;
             }
         }
-
         $usecase = $this->question->usecase;
         $exactmatch = false;
         $engine = $this->question->engine;
         $notation = $this->question->notation;
-        $hinttitlestring = $renderer->render_hint_title(get_string('teststringshintexplanation', 'qtype_writeregex',
-            get_string('hinttitleadditionformode_' . $this->mode, 'qtype_writeregex')));
+        return new \qtype_preg_regex_testing_tool($regex, $strings, $usecase, $exactmatch, $engine,
+            $notation, new \qtype_preg_position());
+    }
 
-        switch($this->mode){
-            case 1:
-                $regex = $response['answer'];
-                $tool = new \qtype_preg_regex_testing_tool($regex, $strings, $usecase, $exactmatch, $engine,
-                    $notation, new \qtype_preg_position());
-                $json = $tool->generate_json();
-                return $hinttitlestring . $json['regex_test'];
-            case 2:
-                $answer = $this->question->get_best_fit_answer($response);
-                $regex = $answer['answer']->answer;
-                $tool = new \qtype_preg_regex_testing_tool($regex, $strings, $usecase, $exactmatch, $engine,
-                    $notation, new \qtype_preg_position());
-                $json = $tool->generate_json();
-                return $hinttitlestring . $json['regex_test'];
-            case 3:
-                $studentsregex = $response['answer'];
-                $tool = new \qtype_preg_regex_testing_tool($studentsregex, $strings, $usecase, $exactmatch, $engine,
-                    $notation, new \qtype_preg_position());
-                $studentsjson = $tool->generate_json();
-                $answer = $this->question->get_best_fit_answer($response);
-                $correctregex = $answer['answer']->answer;
-                $tool = new \qtype_preg_regex_testing_tool($correctregex, $strings, $usecase, $exactmatch, $engine,
-                    $notation, new \qtype_preg_position());
-                $correctjson = $tool->generate_json();
-                return $hinttitlestring . $renderer->generate_teststring_hint_result_table($studentsjson['regex_test'], $correctjson['regex_test']);
-            default:
-                return '';
-        }
+    /**
+     * Render hint for concrete regex.
+     * @param string $regex regex for which hint is to be shown
+     * @return string hint display result for given regex.
+     */
+    public function render_hint_for_answer($answer) {
+        $tree = $this->tool($answer);
+        $json = $tree->generate_json();
+        return $json['regex_test'];
+    }
+
+    /**
+     * Render hint for both students and teachers answers.
+     * @param string $studentsanswer students answer
+     * @param string $teachersanswer teachers answer
+     * @param question $renderer
+     * @return string hint display result for given answers.
+     */
+    public function render_hint_for_both_students_and_teachers_answers($studentsanswer, $teachersanswer, $renderer) {
+        $hintforstudent = $this->render_hint_for_answer($studentsanswer);
+        $hintforteacher = $this->render_hint_for_answer($teachersanswer);
+        return $renderer->generate_teststring_hint_result_table($hintforstudent, $hintforteacher);
     }
 }
