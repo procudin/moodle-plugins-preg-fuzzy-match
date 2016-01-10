bc6e60a11c4d8b39bcf115b36c129dcac78e131d
gvterechov <grvlter@gmail.com>
2016-01-10 23:50:54 +0300

    ???????? ?????? ?? ???????? ??????? ? ??????????? ? ??????

--- blocks/regex_constructor/version.php
+++ blocks/regex_constructor/version.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Preg project - https://code.google.com/p/oasychev-moodle-plugins/
+// This file is part of Preg project - https://bitbucket.org/oasychev/moodle-plugins/overview
 //
 // Preg project is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
