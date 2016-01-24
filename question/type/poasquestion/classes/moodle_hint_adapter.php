9b03b34ec67214dcfefced30253a97b5619f9b8f
oasychev <oasychev@gmail.com>
2016-01-24 22:34:08 +0300

    ???????? ?????? ?? ???????? ??????? ? poasquestion

--- question/type/poasquestion/classes/moodle_hint_adapter.php
+++ question/type/poasquestion/classes/moodle_hint_adapter.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Poasquestion question type - https://code.google.com/p/oasychev-moodle-plugins/
+// This file is part of Poasquestion question type - https://bitbucket.org/oasychev/moodle-plugins/overview
 //
 // Poasquestion question type is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
