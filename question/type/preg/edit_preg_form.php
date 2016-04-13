8b85916acd531361f3c47b6de5750cceab15d7e3
oasychev <oasychev@gmail.com>
2016-04-14 01:54:04 +0300

    ????????? ??????? get_matching_options ? get_matcher ??? ????????? caller'? ?????????? ????? ????? ????????? ???????.

--- question/type/preg/edit_preg_form.php
+++ question/type/preg/edit_preg_form.php
@@ -233,8 +233,8 @@ class qtype_preg_edit_form extends qtype_shortanswer_edit_form {
             if ($trimmedanswer !== '') {
                 $hintused = ($data['usecharhint'] || $data['uselexemhint']) && $fractions[$key] >= $data['hintgradeborder'];
                 // Create matcher to check regex for errors and try to match correct answer.
-                $matcher = $questionobj->get_matcher($data['engine'], $trimmedanswer, $data['exactmatch'],
-                        $questionobj->get_modifiers($data['usecase']), (-1)*$i, $data['notation'], $hintused);
+                $options = $questionobj->get_matching_options($data['exactmatch'], $questionobj->get_modifiers($data['usecase']), (-1)*$i, $data['notation']);
+                $matcher = $questionobj->get_matcher($data['engine'], $trimmedanswer, $options, (-1)*$i, $hintused);
                 if ($matcher->errors_exist()) {// There were errors in the matching process.
                     $regexerrors = $matcher->get_error_messages();// Show no more than max errors.
                     $errors['answer['.$key.']'] = '';
