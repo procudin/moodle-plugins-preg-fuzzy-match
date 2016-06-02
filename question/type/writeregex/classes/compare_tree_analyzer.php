de26f81a3ffbd91724c73e716bea4c992625db2c
Kamo Spertsian <Kamo Spertsian@localhost>
2016-06-02 13:46:59 +0300

    ?????????? ???????? ? ????????????.

--- question/type/writeregex/classes/compare_tree_analyzer.php
+++ question/type/writeregex/classes/compare_tree_analyzer.php
@@ -30,7 +30,7 @@ class compare_tree_analyzer extends analyzer {
     /**
      * Get equality for user response.
      * @param $answer string Regex answer.
-     * @param $respose string User response.
+     * @param $response string User response.
      * @return compare_tree_analyzer_result Result of compare.
      */
     public function analyze ($answer, $respose) {
